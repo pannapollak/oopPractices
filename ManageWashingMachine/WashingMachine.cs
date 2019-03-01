796254f85ca646c14e9b7509dec137317457fda9
Anna Pollák <tintagomba@yahoo.co.uk>
2019-03-01 19:12:43 +0100

    base class in progress

--- ManageWashingMachine/WashingMachine.cs
+++ ManageWashingMachine/WashingMachine.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace ManageWashingMachine
 {
-    class Program
+    class WashingMachine
     {
+        public List<PieceOfClothing> Clothes { get; set; }
+        public List<PieceOfClothing> CurrentLoad { get; set; }
+
+        public bool CheckColor()
+        {
+            return true;
+        }
+
+        public void CheckWeight()
+        {
+        }
+
         static void Main(string[] args)
         {
+            WashingMachine wash = new WashingMachine();
+            wash.Build();
+            wash.Run();
+        }
+
+        private void Build()
+        {
+            Clothes = new List<PieceOfClothing>
+            {
+            }
+        }
+
+        private void Run()
+        {
+            throw new NotImplementedException();
         }
     }
 }
