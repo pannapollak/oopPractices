356b7f84ff216fa58eeca050f384027595eef692
Anna Pollák <tintagomba@yahoo.co.uk>
2019-03-02 01:20:54 +0100

    minimum load implementation, WeightCounter fixed

--- ManageWashingMachine/WashingMachine.cs
+++ ManageWashingMachine/WashingMachine.cs
@@ -8,15 +8,16 @@ namespace ManageWashingMachine
 {
     class WashingMachine
     {
-        
+        public int WeightCounter = 0;
         public List<PieceOfClothing> Clothes { get; set; }
         public List<PieceOfClothing> CurrentLoad { get; set; }
 
-        public void CheckWeight(PieceOfClothing cloth, int weightCounter)
+        public void CheckWeight(PieceOfClothing cloth)
         {
-            if (cloth.Weight + weightCounter <= 5000)
+            if (cloth.Weight + WeightCounter <= 5000)
             {
                 CurrentLoad.Add(cloth);
+                WeightCounter += cloth.Weight;
             }
         }
 
@@ -33,12 +34,15 @@ namespace ManageWashingMachine
         {
             Clothes = new List<PieceOfClothing>
             {
-                new PieceOfClothing("Jumper", "light", 500),
-                new PieceOfClothing("Jumper", "dark", 300),
-                new PieceOfClothing("Leggings", "white", 150),
+                new PieceOfClothing("Jumper", "light", 550),
+                new PieceOfClothing("Jumper", "dark", 650),
+                new PieceOfClothing("Leggings", "white", 250),
                 new PieceOfClothing("T-shirt", "red", 100),
                 new PieceOfClothing("Jeans", "dark", 500),
-                new PieceOfClothing("Trousers", "dark", 400)
+                new PieceOfClothing("Trousers", "dark", 800),
+                new PieceOfClothing("T-shirt", "dark", 300),
+                new PieceOfClothing("Leggings", "dark", 250),
+                new PieceOfClothing("Jeans", "dark", 750)
             };
 
             CurrentLoad = new List<PieceOfClothing>();
@@ -47,22 +51,30 @@ namespace ManageWashingMachine
 
         private void Run(string currentColor)
         {
-            int weightCounter = 0;
             foreach (var cloth in Clothes)
             {
-                if (cloth.Color == currentColor && weightCounter < 5000)
+                if (cloth.Color == currentColor && WeightCounter < 5000)
                 {
-                    CheckWeight(cloth, weightCounter);
+                    CheckWeight(cloth);
                 }
                  
             }
+            
         }
 
         private void PrintResult()
         {
-            foreach (var gettingCleanPieceOfClothing in CurrentLoad)
+            if (WeightCounter >= 2000)
             {
-                Console.WriteLine(gettingCleanPieceOfClothing);
+                foreach (var gettingCleanPieceOfClothing in CurrentLoad)
+                {
+                    Console.WriteLine(gettingCleanPieceOfClothing);
+                }
+            }
+            else
+            {
+                double currentWeight = (double)WeightCounter / (double)1000;
+                Console.WriteLine($"Current load is {currentWeight} kg. Not enough dirty clothes for the load.");
             }
         }
     }
